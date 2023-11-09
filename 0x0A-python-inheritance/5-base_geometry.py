0fd90f97f8ae5c744f1d56d8e028d6a72aa37633
amutsamoses <amutsamoses@gmail.com>
2023-11-09 01:03:22 -0800

    Base Geometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash/python3
+"""defines a base class
+"""
+
+class BaseGeometry:
+    """An empty class"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash/python3
+"""defines base class geometry"""
+
+class BaseGeometry:
+    """based on 5-base_geometry.py"""
+    def area (self):
+        raise Exception('area() is not implemented')
